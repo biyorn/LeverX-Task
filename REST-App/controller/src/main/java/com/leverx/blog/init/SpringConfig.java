60e4730417289a8cdd96a5b94804241c71acdffb
Pavel <porlovski3@gmail.com>
2020-05-31 20:11:23 +0300

    Add UserController and init classe for spring

---
+++ REST-App/controller/src/main/java/com/leverx/blog/init/SpringConfig.java
@@ -0,0 +1,10 @@
+package com.leverx.blog.init;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class SpringConfig implements WebMvcConfigurer {
+}
